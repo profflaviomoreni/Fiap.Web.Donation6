2d896ea83804b1d4727cb3e14e12afada0bc994c
Flavio Moreni <PF0954@fiap.com.br>
2026-01-06 21:19:31 -0300

    edicao de produto com comentário

--- Fiap.Web.Donation6/Controllers/ProdutoController.cs
+++ Fiap.Web.Donation6/Controllers/ProdutoController.cs
@@ -7,9 +7,46 @@ namespace Fiap.Web.Donation6.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var produtos = ListarProdutosMock();
+            return View(produtos);
         }
 
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            // SELECT * FROM produto WHERE ProdutoId = id
+            // Carregar no objeto ProdutoModel os dados banco
+            var produto = ListarProdutosMock().Where(p => p.ProdutoId == id).FirstOrDefault();
+
+            return View(produto);
+        }
+
+
+        [HttpPost]
+        public IActionResult Edit(ProdutoModel produtoModel)
+        {
+            
+            if (string.IsNullOrEmpty(produtoModel.SugestaoTroca))
+            {
+                ViewBag.ErrorMessage = "A sugestão de troca é obrigatória.";
+                return View(produtoModel);
+
+            } else
+            {
+                // UPDATE produto SET ... WHERE ProdutoId = produtoModel.ProdutoId
+                // Criar uma mensagem de sucesso
+                // Exibir a tela dizendo que tivemos sucesso
+
+                ViewBag.SuccessMessage = $"Produto {produtoModel.NomeProduto} atualizado com sucesso!";
+
+                var produtos = ListarProdutosMock();
+
+                return View("Index", produtos);
+
+            }
+
+
+        }
 
 
 
@@ -22,6 +59,7 @@ namespace Fiap.Web.Donation6.Controllers
                 {
                     ProdutoId = 1,
                     NomeProduto = "Iphone 11",
+                    SugestaoTroca = "Descrição da troca",
                     CategoriaId = 1,
                     Disponivel = true,
                     DataExpiracao = DateTime.Now,
